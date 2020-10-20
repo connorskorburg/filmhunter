9f85908e0c3c6a407c18aa282c348e8d44b34ce4
Connor <connorskorburg@gmail.com>
2020-10-20 10:46:18 -0500

    home page is responsive

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import icon from './images/icon.svg';
 
 const Navbar = () => {
   return (
     <nav className='shadow-3 bg-primary top-nav flex'>
-      <h1 className='text-secondary-light'>Film Hunter</h1>
-      <section className="flex-inline text-white font-bold ml-auto">
+      <section>
+        <img src={icon} alt="" />
+        <h1 className='text-secondary-light d-inline-block' style={{ verticalAlign: 'top', marginLeft: '8px' }}>Film Hunter</h1>
+      </section>
+      <section className="flex-inline text-white font-bold ml-auto nav-right">
         <p className='ml-2'>Home</p>
         <p className='ml-2'>Movies</p>
         <p className='ml-2'>TV Shows</p>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,41 +3,30 @@ import hero from '../layout/images/hero.svg';
 
 const Home = () => {
   return (
-    <div className='flex' style={containerStyle} >
-      <main style={sectionStyle}>
-        <section className="w-50 pr-2" style={{ verticalAlign: 'top' }}>
+    <div className='flex hero-outer' >
+      <main className='hero-inner'>
+        <section className="w-50" style={{ verticalAlign: 'top' }}>
           <h1 className='text-primary heading mb-2'>Find Your Favorite Films and Shows</h1>
-          <p className="info text-primary-dark font-bold mb-1">Search for most popular and latest Movies and Television shows</p>
+          <p className="info text-secondary-dark font-bold mb-2">Search for most popular and latest Movies and Television shows</p>
           <div className="btn-container">
             <button className="btn btn-secondary mr-1">Movies</button>
             <button className="btn btn-primary">TV Shows</button>
           </div>
         </section>
         <section style={heroStyle} className='w-50'>
-          <img src={hero} alt="" style={{ width: '85%', marginLeft: '15%', display: 'inline-block' }} />
+          <img src={hero} alt="" className='hero-img' />
+          <article className="hero-sm">
+            <p className="text-secondary-dark font-bold my-2 ta-center font-l">Search for most popular and latest Movies and Television shows</p>
+            <button className="btn btn-block btn-secondary mb-1">Movies</button>
+            <button className="btn btn-block btn-primary">TV Shows</button>
+          </article>
         </section>
       </main>
     </div>
   )
 }
 
-const containerStyle = {
-  position: 'relative',
-  height: '100vh',
-}
-
-const sectionStyle = {
-  padding: '100px 5% 0 5%',
-  width: '100%',
-  height: 'fit-content',
-  overflow: 'hidden',
-  margin: 'auto 0',
-  position: 'absolute',
-  top: '0',
-  bottom: '100px',
-  right: '0',
-  left: '0',
-}
+// style = {{ width: '85%', marginLeft: '15%', display: 'inline-block' }}
 
 const heroStyle = {
   flex: '1',
