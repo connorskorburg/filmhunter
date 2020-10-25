06c1f9c08e094cf71d2cd0dd8c32a5f87b02ca82
Connor <connorskorburg@gmail.com>
2020-10-25 10:46:13 -0500

    updated movie card styling

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, Fragment } from 'react';
 import MovieContext from '../../context/movie/movieContext';
 import Loading from '../layout/Loading';
 
@@ -37,56 +37,72 @@ const Movie = ({ match: { params: { id } } }) => {
 		content = <Loading />;
 	} else {
 		content = (
-			<div className="container flex pt-1 text-white">
-				<section className="pt-4 card bg-primary mb-1" style={{ height: 'fit-content' }}>
-					<p className="sub-heading font-bold text-secondary mb-2" style={{ margin: '0' }}>
-						{original_title}
-					</p>
-					{tagline !== '' ? <p className="info mt-2 mb-1">"{tagline}"</p> : ''}
-					<p className="info mt-2 mb-4">{overview}</p>
-					<article>
-						<p className="info font-bold text-secondary mb-2">Production Companies</p>
-						<ul>
-							{production_companies &&
-								production_companies.map((p) => {
-									return (
-										<li className="mb-1 btn font-l bg-secondary mr-1" key={p.id}>
-											{p.name}
-										</li>
-									);
-								})}
-						</ul>
-						{homepage !== '' ? (
-              <>
-                <p className="text-xl font-bold mb-2">Visit Home Page</p>
-                <a className="btn btn-secondary" href={homepage}>
-                  {original_title} Homepage
-                </a>
-              </>
-						) : (
-							''
-						)}
-						<div className="flex ta-center mt-2">
-							<div className="w-50 card bg-white mr-1">
-								<p className="info text-primary font-bold mb-1">Rating</p>
-								<p className="font-l btn bg-white text-secondary">{vote_average}/10</p>
+			<main style={{ backgroundColor: '#ecf5f9' }}>
+				<div className="container flex pt-1 text-primary">
+					<section className="mb-1" style={{ height: 'fit-content' }}>
+						<article
+							className="card mb-1 shadow-4 bg"
+							style={{ border: '0.5px dotted var(--primary)', backgroundColor: '#8FA6B0' }}
+						>
+							<p className="sub-heading font-bold text-white mb-2" style={{ margin: '0' }}>
+								{original_title}
+							</p>
+							{tagline !== '' ? (
+								<p className="text-head text-secondary-dark font-bold mt-2 mb-1">"{tagline}"</p>
+							) : (
+								''
+							)}
+							<p className="text-head mt-2 mb-4">{overview}</p>
+						</article>
+						<article
+							className="card shadow-4 bg"
+							style={{ border: '0.5px dotted var(--primary)', backgroundColor: '#8FA6B0' }}
+						>
+							<div className="flex ta-center mb-2">
+								<div className="w-50 mr-1 bg" style={{ border: '1px dotted var(--primary)' }}>
+									<p className="text-head text-primary font-bold my-1">Rating</p>
+									<p className="font-l btn bg-primary text-white mb-1">{vote_average}/10</p>
+								</div>
+								<div className="w-50 bg" style={{ border: '1px dotted var(--primary)' }}>
+									<p className="text-head text-primary font-bold my-1">Status</p>
+									<p className="font-l btn bg-primary text-white mb-1">{status}</p>
+								</div>
 							</div>
-							<div className="w-50 card bg-white">
-								<p className="info text-primary font-bold mb-1">Status</p>
-								<p className="font-l btn bg-white text-secondary">{status}</p>
-							</div>
-						</div>
-					</article>
-				</section>
-				<section className="pb-1 pl-2 ml-auto" style={{ width: 'auto' }}>
-					<img
-						style={{ width: 'auto', maxHeight: 'calc(95vh - 5rem)' }}
-						className={`d-block ml-auto poster-img poster-img-${id}`}
-						src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-						alt={original_title}
-					/>
-				</section>
-			</div>
+							<ul>
+								{production_companies && (
+									<p className="text-head font-bold text-white mb-2">Production Companies</p>
+								)}
+								{production_companies &&
+									production_companies.map((p) => {
+										return (
+											<li className="mb-1 btn text-white font-l bg-secondary mr-1" key={p.id}>
+												{p.name}
+											</li>
+										);
+									})}
+							</ul>
+							{homepage !== '' ? (
+								<Fragment>
+									<p className="text-head text-white font-bold my-2">Visit Home Page</p>
+									<a className="btn btn-secondary" href={homepage}>
+										{original_title} Homepage
+									</a>
+								</Fragment>
+							) : (
+								''
+							)}
+						</article>
+					</section>
+					<section className="pb-1 pl-2 ml-auto" style={{ width: 'auto' }}>
+						<img
+							style={{ width: 'auto', maxHeight: 'calc(95vh - 5rem)' }}
+							className={`d-block ml-auto poster-img poster-img-${id}`}
+							src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+							alt={original_title}
+						/>
+					</section>
+				</div>
+			</main>
 		);
 	}
 
