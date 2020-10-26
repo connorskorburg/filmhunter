be2453c3ad0ab55e833b6f46a63c63c8222e5eb5
Connor <connorskorburg@gmail.com>
2020-10-26 18:23:08 -0500

    updated movie card styling

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -14,14 +14,11 @@ const Movie = ({ match: { params: { id } } }) => {
 	useEffect(
 		() => {
 			setLoading();
-			console.log(id);
 			getMovie(id);
 		},
 		//eslint-disable-next-line
 		[ id ]
 	);
-
-	let content;
 
 	const {
 		original_title,
@@ -34,46 +31,41 @@ const Movie = ({ match: { params: { id } } }) => {
 		vote_average
 	} = movie;
 
+	let content;
+
 	if (loading) {
 		content = <Loading />;
 	} else {
 		content = (
-			<main style={{ backgroundColor: '#ecf5f9' }}>
-				<div className="container flex pt-1 text-primary">
-					<section className="mb-1" style={{ height: 'fit-content', width: '60%' }}>
-						<article
-							className="card mb-1 shadow-4 bg b-dotted-primary"
-							style={{ backgroundColor: '#8FA6B0' }}
-						>
-							<p className="sub-heading pt-1 font-bold text-white mb-2" style={{ margin: '0' }}>
-								{original_title}
-							</p>
-							{tagline !== '' ? (
-								<p className="text-head text-secondary-dark font-bold mt-2 mb-1">"{tagline}"</p>
-							) : (
-								''
-							)}
-							<p className="text-head mt-2 mb-2">{overview}</p>
-						</article>
-						<article className="card shadow-4 bg b-dotted-primary" style={{ backgroundColor: '#8FA6B0' }}>
-							<ProductionCompanies production_companies={production_companies} />
-							<Homepage original_title={original_title} homepage={homepage} />
-							<RatingStatus status={status} vote_average={vote_average} />
-						</article>
-					</section>
-					<section className="pb-1 pl-2 ml-auto" style={{ width: '40%' }}>
-						<img
-							style={{ width: '100%' }}
-							className={`d-block ml-auto poster-img poster-img-${id}`}
-							src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-							alt={original_title}
-						/>
-					</section>
-				</div>
-			</main>
+			<div className="container flex pt-1 text-primary movie-card">
+				<section className="mb-1 w-60" style={{ height: 'fit-content' }}>
+					<article className="card mb-1 shadow-4 bg-primary-light b-dotted-primary">
+						<p className="sub-heading pt-1 font-bold text-white mb-2" style={{ margin: '0' }}>
+							{original_title}
+						</p>
+						{tagline !== '' ? (
+							<p className="text-head text-secondary-dark font-bold mt-2 mb-1">"{tagline}"</p>
+						) : (
+							''
+						)}
+						<p className="text-head mt-2 mb-2">{overview}</p>
+					</article>
+					<article className="card shadow-4 bg-primary-light b-dotted-primary">
+						<ProductionCompanies production_companies={production_companies} />
+						<Homepage original_title={original_title} homepage={homepage} />
+						<RatingStatus status={status} vote_average={vote_average} />
+					</article>
+				</section>
+				<section className="pb-1 img-section ml-auto w-40">
+					<img
+						className={`w-100 d-block ml-auto poster-img poster-img-${id}`}
+						src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+						alt={original_title}
+					/>
+				</section>
+			</div>
 		);
 	}
-
 	return <div className="pt-4">{content}</div>;
 };
 
