650f3ed9972567cfe2e2b44b483710d87bb1d9d3
Connor <connorskorburg@gmail.com>
2020-10-29 20:53:38 -0500

    updated responsiveness

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,17 +4,24 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='shadow-3 bg-primary top-nav flex'>
-      <section>
-        <img src={icon} alt="" />
-        <h1 className='text-secondary-light d-inline-block' style={{ verticalAlign: 'top', marginLeft: '8px' }}>Film Hunter</h1>
-      </section>
-      <section className="flex-inline text-white font-bold ml-auto nav-right">
-        <Link to="/" className='text-m text-white ml-2'>Home</Link>
-        <Link to="/movies" className='text-m text-white ml-2'>Movies</Link>
-        <Link to="/shows" className='text-m text-white ml-2'>TV Shows</Link>
-      </section>
-    </nav>
+    <main>
+      <nav className='shadow-3 bg-primary top-nav flex'>
+        <section>
+          <img src={icon} alt="" />
+          <h1 className='text-secondary-light d-inline-block' style={{ verticalAlign: 'top', marginLeft: '8px' }}>Film Hunter</h1>
+        </section>
+        <section className="flex-inline text-white font-bold ml-auto nav-right">
+          <Link to="/" className='text-m text-white ml-2'>Home</Link>
+          <Link to="/movies" className='text-m text-white ml-2'>Movies</Link>
+          <Link to="/shows" className='text-m text-white ml-2'>TV Shows</Link>
+        </section>
+      </nav>
+      <nav className="bottom-nav">
+        <Link to="/" className='text-m text-white bottom-nav-link'>Home</Link>
+        <Link to="/movies" className='text-m text-white bottom-nav-link'>Movies</Link>
+        <Link to="/shows" className='text-m text-white bottom-nav-link'>TV Shows</Link>
+      </nav>
+    </main>
   )
 }
 
